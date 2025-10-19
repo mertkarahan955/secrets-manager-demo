5d34eb20fecd5a20c480f45e5ac15c7053ca100a
mert karahan <mkarahan@eseye.com>
2025-10-19 17:16:17 +0300

    tfstate s3 created

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,2 +1,10 @@
-# Backend configuration will be configured after initial setup
-# Run: terraform init -backend-config="bucket=BUCKET_NAME"
+terraform {
+  backend "s3" {
+    bucket         = "secrets-manager-poc-tfstate-56ad11fb"
+    key            = "terraform.tfstate"
+    region         = "eu-west-1"
+    encrypt        = true
+    kms_key_id     = "alias/terraform-state-key"
+    dynamodb_table = "terraform-state-lock"
+  }
+}
